a4f6daeedc46c78c840f7c05eb6f1c6ffb394ac9
Elias Baldwin <eliasab@uw.edu>
2021-12-07 16:23:39 -0800

    day1, plus prep for further days

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,23 @@
+private const val day = 1
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return input.windowed(size = 2, transform = {(it[1] > it[0]).toInt()}).sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        return part1(input.windowed(size = 3, transform = { it.sum() }))
     }
+
+    fun parseData(input: String) =
+        input.split("\n").map { it.toInt() }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput(day, true)
+    check(part1(parseData(testInput)) == 7)
+    check(part2(parseData(testInput)) == 5)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput(day)
+    println(part1(parseData(input)))
+    println(part2(parseData(input)))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -5,9 +5,14 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readInput(dayNum: Int, test: Boolean = false) : String {
+    val fname = dayNum.toString().padStart(2, '0') + (if (test) "_test" else "")
+    return File("src/input", "${fname}.txt").readText()
+}
 
 /**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+fun Boolean.toInt(): Int = if (this) 1 else 0
